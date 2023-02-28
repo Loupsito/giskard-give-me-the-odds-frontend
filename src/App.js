2d3328102bf9e9150beb1eecf6f7d7790bbe6054
AlexandreS <virya.suy@gmail.com>
2023-02-28 01:01:38 +0100

    feat: add button to calculate odds

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [file, setFile] = useState(null);
   const [title, setTitle] = useState("");
+  const [odds, setOdds] = useState("");
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     setFile(file);
     setTitle(file.name);
-  }
+    setOdds("");
+  };
+
+  const handleButtonClick = () => {
+    setOdds("0");
+  };
 
   return (
-      <div className="upload-container">
-        <h1>Upload json file</h1>
-        <input aria-label="inputJsonFile" type="file" accept=".json" onChange={handleFileChange} />
-        <div className="result">
-          {title && <h2>File Title: {title}</h2>}
-        </div>
+    <div className="upload-container">
+      <h1>Upload json file</h1>
+      <input
+        aria-label="inputJsonFile"
+        type="file"
+        accept=".json"
+        onChange={handleFileChange}
+      />
+      {title && !odds &&<button onClick={handleButtonClick}>Give me the odds</button>}
+      <div className="result">
+        {odds && <p>{odds}%</p>}
       </div>
+    </div>
   );
 }
 
